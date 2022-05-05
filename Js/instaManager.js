8a8f239653273a1a19c4b717c981da39e723e997
Daniel Sávio <spnjc137@gmail.com>
2022-05-04 22:47:10 -0300

    Linkando com o instagram e fazendo a parte de notícias

---
+++ Js/instaManager.js
@@ -0,0 +1,11 @@
+// IGQVJXMEVJd29oWnFubGl1dVVRQXY4aTRBd09qal9tU3VucGo1ZA3JWWnFXRUlIM1BXdXU1WFd2SnVSUlBfS19lbWtHRWpoUzVWZAUx3SVRDMUt3UjJlYlV5X1ZA0WDRJcW45REk2YjJhVUtZAV1Fjek5KWgZDZD
+
+
+    let feed = new Instafeed({
+      accessToken: 'IGQVJXMEVJd29oWnFubGl1dVVRQXY4aTRBd09qal9tU3VucGo1ZA3JWWnFXRUlIM1BXdXU1WFd2SnVSUlBfS19lbWtHRWpoUzVWZAUx3SVRDMUt3UjJlYlV5X1ZA0WDRJcW45REk2YjJhVUtZAV1Fjek5KWgZDZD',
+      limit:3,
+      template: '<div class="insta-card"><img src="{{image}}" class="insta-img">  <div class="overlay-left"> <a href="{{link}}"><div class="text">Ver na rede social</div></a> </div> <div class="overlay-rigth"> <a href="./contato.html"><div class="text">Entrar em contato</div></a>  </div></div>'
+    
+    
+    });
+    feed.run();
